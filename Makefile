6f34c02eff0db92c246c42b9cded28b08dfdd642
Ian Douglas Scott <idscott@system76.com>
2022-07-28 12:45:07 -0700

    Initial commit
    WIP screenshot portal

---
+++ Makefile
@@ -0,0 +1,54 @@
+prefix ?= /usr/local
+bindir = $(prefix)/bin
+libdir = $(prefix)/lib
+libexecdir = $(prefix)/libexec
+includedir = $(prefix)/include
+datarootdir = $(prefix)/share
+datadir = $(datarootdir)
+
+TARGET = debug
+DEBUG ?= 0
+ifeq ($(DEBUG),0)
+	TARGET = release
+	ARGS += --release
+endif
+
+VENDOR ?= 0
+ifneq ($(VENDOR),0)
+	ARGS += --frozen
+endif
+
+BIN = xdg-desktop-portal-cosmic
+DBUS_NAME = org.freedesktop.impl.portal.desktop.cosmic
+
+all: $(BIN)
+
+clean:
+	rm -rf target
+
+distclean: clean
+	rm -rf .cargo vendor vendor.tar
+
+$(BIN): Cargo.toml Cargo.lock src/main.rs vendor-check
+	cargo build $(ARGS) --bin ${BIN}
+
+install:
+	install -Dm0755 target/$(TARGET)/$(BIN) $(DESTDIR)$(libexecdir)/$(BIN)
+	install -Dm0644 data/$(BIN).service $(DESTDIR)/$(libdir)/systemd/user/$(BIN).service
+	install -Dm0644 data/$(DBUS_NAME).service $(DESTDIR)/$(datadir)/dbus-1/services/$(DBUS_NAME).service
+	install -Dm0644 data/cosmic.portal $(DESTDIR)/$(datadir)/xdg-desktop-portal/portals/cosmic.portal
+
+## Cargo Vendoring
+
+vendor:
+	rm .cargo -rf
+	mkdir -p .cargo
+	cargo vendor | head -n -1 > .cargo/config
+	echo 'directory = "vendor"' >> .cargo/config
+	tar cf vendor.tar vendor
+	rm -rf vendor
+
+vendor-check:
+ifeq ($(VENDOR),1)
+	rm vendor -rf && tar xf vendor.tar
+endif
---
+++ src/main.rs
@@ -0,0 +1,122 @@
+use std::{collections::HashMap, future};
+use zbus::zvariant;
+
+static DBUS_NAME: &str = "org.freedesktop.impl.portal.desktop.cosmic";
+static DBUS_PATH: &str = "/org/freedesktop/portal/desktop";
+
+const PORTAL_RESPONSE_SUCCESS: u32 = 0;
+const PORTAL_RESPONSE_CANCELLED: u32 = 1;
+const PORTAL_RESPONSE_OTHER: u32 = 2;
+
+// org.freedesktop.impl.portal.Request/org.freedesktop.impl.portal.Session
+// - implemented by objects at different paths
+// org.freedesktop.impl.portal.Inhibit
+// org.freedesktop.impl.portal.Screenshot
+// - save to /run/user/$UID/doc/ with document portal fuse filesystem
+//
+// zbus: implement multiple interfaces at one path?
+
+#[derive(zvariant::DeserializeDict, zvariant::Type)]
+#[zvariant(signature = "a{sv}")]
+struct ScreenshotOptions {
+    modal: Option<bool>,
+    interactive: Option<bool>,
+}
+
+#[derive(zvariant::SerializeDict, zvariant::Type)]
+#[zvariant(signature = "a{sv}")]
+struct ScreenshotResult {
+    uri: String,
+}
+
+#[derive(zvariant::SerializeDict, zvariant::Type)]
+#[zvariant(signature = "a{sv}")]
+struct PickColorResult {
+    color: (f64, f64, f64), // (ddd)
+}
+
+struct Screenshot;
+
+#[zbus::dbus_interface(name = "org.freedesktop.impl.portal.Screenshot")]
+impl Screenshot {
+    async fn screenshot(
+        &self,
+        #[zbus(connection)] connection: &zbus::Connection,
+        handle: zvariant::ObjectPath<'_>,
+        app_id: &str,
+        parent_window: &str,
+        option: ScreenshotOptions,
+    ) -> (u32, ScreenshotResult) {
+        // connection.object_server().at(&handle, Request);
+
+        // TODO create handle
+        // XXX
+        std::fs::copy(
+            "/usr/share/backgrounds/pop/kate-hazen-COSMIC-desktop-wallpaper.png",
+            "/tmp/out.png",
+        );
+
+        // connection.object_server().remove::<Request, _>(&handle);
+        (
+            PORTAL_RESPONSE_SUCCESS,
+            ScreenshotResult {
+                uri: format!("file:///tmp/out.png"),
+            },
+        )
+    }
+
+    async fn pick_color(
+        &self,
+        handle: zvariant::ObjectPath<'_>,
+        app_id: &str,
+        parent_window: &str,
+        option: HashMap<String, zvariant::Value<'_>>,
+    ) -> (u32, PickColorResult) {
+        // TODO create handle
+        // XXX
+        (
+            PORTAL_RESPONSE_SUCCESS,
+            PickColorResult {
+                color: (1., 1., 1.),
+            },
+        )
+    }
+}
+
+/*
+Screenshot (IN  o     handle,
+            IN  s     app_id,
+            IN  s     parent_window,
+            IN  a{sv} options,
+            OUT u     response,
+            OUT a{sv} results);
+
+ressults has uri:string
+
+PickColor  (IN  o     handle,
+            IN  s     app_id,
+            IN  s     parent_window,
+            IN  a{sv} options,
+            OUT u     response,
+            OUT a{sv} results);
+ */
+
+struct Request;
+
+#[zbus::dbus_interface(name = "org.freedesktop.impl.portal.Request")]
+impl Request {
+    fn close(&self) {}
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> zbus::Result<()> {
+    let connection = zbus::ConnectionBuilder::session()?
+        .name(DBUS_NAME)?
+        .serve_at(DBUS_PATH, Screenshot)?
+        .build()
+        .await?;
+
+    future::pending::<()>().await;
+
+    Ok(())
+}
