8d4216466d91766ce4a6e20d131750d738d277f7
Ian Douglas Scott <idscott@system76.com>
2023-03-17 11:09:56 -0700

    Update for changes in cosmic-client-toolkit/sctk

--- src/wayland.rs
+++ src/wayland.rs
@@ -4,16 +4,18 @@ use cosmic_client_toolkit::{
     cosmic_protocols::screencopy::v1::client::{
         zcosmic_screencopy_manager_v1, zcosmic_screencopy_session_v1,
     },
-    screencopy::{BufferInfo, ScreencopyHandler, ScreencopyState},
+    screencopy::{
+        BufferInfo, ScreencopyHandler, ScreencopySessionData, ScreencopySessionDataExt,
+        ScreencopyState,
+    },
     sctk::{
         self,
         output::{OutputHandler, OutputState},
         registry::{ProvidesRegistryState, RegistryState},
-        shm::{ShmHandler, ShmState},
+        shm::{Shm, ShmHandler},
     },
 };
 use std::{
-    collections::HashMap,
     io::Write,
     os::unix::io::{AsFd, AsRawFd, FromRawFd, OwnedFd},
     process,
@@ -21,7 +23,6 @@ use std::{
     thread,
 };
 use wayland_client::{
-    backend::ObjectId,
     globals::registry_queue_init,
     protocol::{wl_buffer, wl_output, wl_shm, wl_shm_pool},
     Connection, Dispatch, Proxy, QueueHandle, WEnum,
@@ -33,7 +34,6 @@ struct WaylandHelperInner {
     qh: QueueHandle<AppData>,
     screencopy_manager: zcosmic_screencopy_manager_v1::ZcosmicScreencopyManagerV1,
     wl_shm: wl_shm::WlShm,
-    sessions: Mutex<HashMap<ObjectId, Session>>,
 }
 
 // TODO seperate state object from what is passed to threads
@@ -47,37 +47,43 @@ struct AppData {
     registry_state: RegistryState,
     screencopy_state: ScreencopyState,
     output_state: OutputState,
-    shm_state: ShmState,
+    shm_state: Shm,
 }
 
 #[derive(Default)]
-struct SessionData {
+struct SessionInner {
     buffer_infos: Option<Vec<BufferInfo>>,
     res: Option<Result<(), WEnum<zcosmic_screencopy_session_v1::FailureReason>>>,
 }
 
 // TODO: dmabuf? need to handle modifier negotation
 #[derive(Default)]
-struct SessionInner {
+struct Session {
     condvar: Condvar,
-    data: Mutex<SessionData>,
+    inner: Mutex<SessionInner>,
 }
 
-#[derive(Clone, Default)]
-struct Session {
-    inner: Arc<SessionInner>,
+#[derive(Default)]
+struct SessionData {
+    session: Arc<Session>,
+    session_data: ScreencopySessionData,
 }
 
 impl Session {
-    fn update<F: FnOnce(&mut SessionData)>(&self, f: F) {
-        f(&mut *self.inner.data.lock().unwrap());
-        self.inner.condvar.notify_all();
+    pub fn for_session(
+        session: &zcosmic_screencopy_session_v1::ZcosmicScreencopySessionV1,
+    ) -> Option<&Self> {
+        Some(&session.data::<SessionData>()?.session)
     }
 
-    fn wait_while<F: FnMut(&SessionData) -> bool>(&self, mut f: F) -> MutexGuard<SessionData> {
-        self.inner
-            .condvar
-            .wait_while(self.inner.data.lock().unwrap(), |data| f(data))
+    fn update<F: FnOnce(&mut SessionInner)>(&self, f: F) {
+        f(&mut *self.inner.lock().unwrap());
+        self.condvar.notify_all();
+    }
+
+    fn wait_while<F: FnMut(&SessionInner) -> bool>(&self, mut f: F) -> MutexGuard<SessionInner> {
+        self.condvar
+            .wait_while(self.inner.lock().unwrap(), |data| f(data))
             .unwrap()
     }
 }
@@ -89,7 +95,7 @@ impl WaylandHelper {
         let qh = event_queue.handle();
         let registry_state = RegistryState::new(&globals);
         let screencopy_state = ScreencopyState::new(&globals, &qh);
-        let shm_state = ShmState::bind(&globals, &qh).unwrap();
+        let shm_state = Shm::bind(&globals, &qh).unwrap();
         let wayland_helper = WaylandHelper {
             inner: Arc::new(WaylandHelperInner {
                 conn,
@@ -97,7 +103,6 @@ impl WaylandHelper {
                 qh: qh.clone(),
                 screencopy_manager: screencopy_state.screencopy_manager.clone(),
                 wl_shm: shm_state.wl_shm().clone(),
-                sessions: Mutex::new(HashMap::new()),
             }),
         };
         let mut data = AppData {
@@ -149,17 +154,17 @@ impl WaylandHelper {
 
         let overlay_cursor = if overlay_cursor { 1 } else { 0 };
 
-        let mut sessions = self.inner.sessions.lock().unwrap();
+        let session = Arc::new(Session::default());
         let screencopy_session = self.inner.screencopy_manager.capture_output(
             &output,
             zcosmic_screencopy_manager_v1::CursorMode::Hidden, // XXX take into account adventised capabilities
             &self.inner.qh,
-            Default::default(),
+            SessionData {
+                session: session.clone(),
+                session_data: Default::default(),
+            },
         );
         self.inner.conn.flush().unwrap();
-        let session = Session::default();
-        sessions.insert(screencopy_session.id(), session.clone());
-        drop(sessions);
 
         let buffer_infos = session
             .wait_while(|data| data.buffer_infos.is_none())
@@ -255,7 +260,7 @@ impl ProvidesRegistryState for AppData {
 }
 
 impl ShmHandler for AppData {
-    fn shm_state(&mut self) -> &mut ShmState {
+    fn shm_state(&mut self) -> &mut Shm {
         &mut self.shm_state
     }
 }
@@ -311,8 +316,7 @@ impl ScreencopyHandler for AppData {
         session: &zcosmic_screencopy_session_v1::ZcosmicScreencopySessionV1,
         buffer_infos: &[BufferInfo],
     ) {
-        let sessions = self.wayland_helper.inner.sessions.lock().unwrap();
-        sessions.get(&session.id()).unwrap().update(|data| {
+        Session::for_session(session).unwrap().update(|data| {
             data.buffer_infos = Some(buffer_infos.to_vec());
         });
     }
@@ -323,8 +327,7 @@ impl ScreencopyHandler for AppData {
         qh: &QueueHandle<Self>,
         session: &zcosmic_screencopy_session_v1::ZcosmicScreencopySessionV1,
     ) {
-        let sessions = self.wayland_helper.inner.sessions.lock().unwrap();
-        sessions.get(&session.id()).unwrap().update(|data| {
+        Session::for_session(session).unwrap().update(|data| {
             data.res = Some(Ok(()));
         });
     }
@@ -337,8 +340,7 @@ impl ScreencopyHandler for AppData {
         reason: WEnum<zcosmic_screencopy_session_v1::FailureReason>,
     ) {
         // TODO send message to thread
-        let sessions = self.wayland_helper.inner.sessions.lock().unwrap();
-        sessions.get(&session.id()).unwrap().update(|data| {
+        Session::for_session(session).unwrap().update(|data| {
             data.res = Some(Err(reason));
         });
     }
@@ -381,7 +383,13 @@ pub fn connect_to_wayland() -> wayland_client::Connection {
     wayland_connection
 }
 
+impl ScreencopySessionDataExt for SessionData {
+    fn screencopy_session_data(&self) -> &ScreencopySessionData {
+        &self.session_data
+    }
+}
+
 sctk::delegate_shm!(AppData);
 sctk::delegate_registry!(AppData);
 sctk::delegate_output!(AppData);
-cosmic_client_toolkit::delegate_screencopy!(AppData);
+cosmic_client_toolkit::delegate_screencopy!(AppData, session: [SessionData]);
